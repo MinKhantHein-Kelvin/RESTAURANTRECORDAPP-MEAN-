094a4b35f2539a8bee6b0de3e10d774b3d2c0036
Min Khant Hein <minkhan1999ucsmub@gmail.com>
2022-09-14 22:35:24 +0630

    v13

--- src/app/shared/restaurant-service.service.ts
+++ src/app/shared/restaurant-service.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantServiceService {
+  public base_Url = 'http://localhost:3000';
 
   constructor(private http : HttpClient) { }
-  public base_Url = environment.API_URL;
+
 
   getRestaurent(){
     return this.http.get(`${this.base_Url}/api/restaurant`).pipe(map((res:any)=>{
