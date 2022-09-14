5a2a264403efd099a4da69da7725d1ea46dc4600
Min Khant Hein <minkhan1999ucsmub@gmail.com>
2022-09-14 22:31:30 +0630

    v12

---
+++ src/app/modal/resturant-data.ts
@@ -0,0 +1,8 @@
+export interface ResturantData {
+  id: number;
+  name: string;
+  email: string;
+  mobile: string;
+  address: string;
+  service: string;
+}
--- src/app/shared/restaurant-service.service.ts
+++ src/app/shared/restaurant-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,10 @@ import { map } from 'rxjs';
 export class RestaurantServiceService {
 
   constructor(private http : HttpClient) { }
+  public base_Url = environment.API_URL;
 
   getRestaurent(){
-    return this.http.get('http://localhost:3000/api/restaurant').pipe(map((res:any)=>{
+    return this.http.get(`${this.base_Url}/api/restaurant`).pipe(map((res:any)=>{
       return res;
     }))
   };
